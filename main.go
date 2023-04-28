c0e3d83cef264d26da2a00d766ee8a23559694db
Craig Pastro <craig.pastro@pangea.cloud>
2023-04-28 13:41:18 -0700

    oops

--- main.go
+++ main.go
@@ -10,10 +10,6 @@ func init() {
 			return sqlite.SQLITE_ERROR, err
 		}
 
-		return sqlite.SQLITE_OK, nil
-	})
-
-	sqlite.Register(func(api *sqlite.ExtensionApi) (sqlite.ErrorCode, error) {
 		if err := api.CreateFunction("url_reputation", &URLReputation{}); err != nil {
 			return sqlite.SQLITE_ERROR, err
 		}
