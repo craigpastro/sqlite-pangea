1b44d48f1ecfb67623cdc5c400b156225fe6b2bd
Craig Pastro <craig.pastro@pangea.cloud>
2023-04-27 21:26:40 -0700

    who knows

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"testing"
@@ -11,16 +10,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const extension = "pangea.so"
-
 func TestRedact(t *testing.T) {
 	token, ok := os.LookupEnv("PANGEA_TOKEN")
 	if !ok {
 		t.Fatal("please set the PANGEA_TOKEN environment variable")
 	}
 
-	if _, err := os.Stat(extension); errors.Is(err, os.ErrNotExist) {
-		t.Fatal("'pangea.so' does not exist; perhaps you need to build it?")
+	extension, ok := os.LookupEnv("PANGEA_EXTENSION")
+	if !ok {
+		t.Fatal("please set the PANGEA_EXTENSION environment variable to the location of the extension")
 	}
 
 	conn, err := sqlite.OpenConn("file::memory:", 0)
